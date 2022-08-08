8a258165cd8eeda8973f7e76e2d772edab204a69
Šimon Brauner <simonbrauner@seznam.cz>
2022-08-08 21:39:48 +0200

    04 shorter

--- 04.py
+++ 04.py
@@ -1,66 +1,51 @@
-from typing import Dict, List, Optional, Tuple
-from collections import defaultdict
+from typing import List
 
 
-def board_sum(boards: List[List[List[int]]], index: int) -> int:
+def remove_from_boards(number: int, boards: List[List[List[int]]]) -> None:
+    for board in boards:
+        for row in board:
+            for x in range(5):
+                if row[x] == number:
+                    row[x] = -1
+
+
+def board_won(board: List[List[int]]) -> bool:
+    for i in range(5):
+        if all([board[i][j] == -1 for j in range(5)]) or all(
+            [board[j][i] == -1 for j in range(5)]
+        ):
+            return True
+
+    return False
+
+
+def board_sum(board: List[List[int]]) -> int:
     result = 0
 
-    for row in boards[index]:
+    for row in board:
         for number in row:
-            if number > 0:
+            if number != -1:
                 result += number
 
     return result
 
 
-def board_won(
-    boards: List[List[List[int]]],
-    board: Tuple[int, int, int],
-) -> bool:
-    return all([boards[board[0]][board[1]][x] < 0 for x in range(5)]) or all(
-        [boards[board[0]][y][board[2]] < 0 for y in range(5)]
-    )
-
-
-def draw_number(
-    boards: List[List[List[int]]],
-    positions: Dict[int, List[Tuple[int, int, int]]],
-    drawn: int,
-) -> List[Optional[int]]:
-    won: List[Optional[int]] = [None]
-
-    for board in positions[drawn]:
-        boards[board[0]][board[1]][board[2]] = -1
-
-        if board_won(boards, board):
-            if won[0] is None:
-                won[0] = board[0]
-            else:
-                won.append(board[0])
-
-    return won
-
-
 def play(
     draws: List[int],
     boards: List[List[List[int]]],
-    positions: Dict[int, List[Tuple[int, int, int]]],
     first: bool,
 ) -> int:
-    won_already = []
-
     for drawn in draws:
-        draw_result = draw_number(boards, positions, drawn)
+        remove_from_boards(drawn, boards)
 
-        if first and draw_result[0] is not None:
-            return board_sum(boards, draw_result[0]) * drawn
+        won = [x for x in boards if board_won(x)]
+        boards = [x for x in boards if not board_won(x)]
 
-        won_already.extend([x for x in draw_result if x is not None])
-        for key in positions.keys():
-            positions[key] = [x for x in positions[key] if x[0] not in draw_result]
+        if first and len(won) > 0:
+            return board_sum(won[0]) * drawn
 
-        if len(won_already) == len(boards):
-            return board_sum(boards, won_already[-1]) * drawn
+        if not first and len(boards) == 0:
+            return board_sum(won[-1]) * drawn
 
     assert False
 
@@ -68,16 +53,12 @@ def play(
 with open("data.txt") as f:
     draws = [int(x) for x in f.readline().split(",")]
     boards: List[List[List[int]]] = []
-    positions: Dict[int, List[Tuple[int, int, int]]] = defaultdict(list)
 
     while f.readline():
         board = []
         for y in range(5):
-            row = [int(x) for x in f.readline().split()]
-            for x in range(5):
-                positions[row[x]].append((len(boards), y, x))
-            board.append(row)
+            board.append([int(x) for x in f.readline().split()])
         boards.append(board)
 
-    print(play(draws, boards, positions, True))
-    print(play(draws, boards, positions, False))
+    print(play(draws, boards, True))
+    print(play(draws, boards, False))

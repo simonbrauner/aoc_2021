50cd304c9bf2fefbab37d9e385ac36e98c42351a
Šimon Brauner <simonbrauner@seznam.cz>
2022-08-09 19:39:10 +0200

    10 part 2

--- 10.py
+++ 10.py
@@ -1,8 +1,9 @@
 from typing import List
 
 
-OPENING_DELIMITERS = {"(": ")", "[": "]", "{": "}", "<": ">"}
-CLOSING_DELIMITERS = {")": 3, "]": 57, "}": 1197, ">": 25137}
+OPENING = {"(": ")", "[": "]", "{": "}", "<": ">"}
+CLOSING_SYNTAX = {")": 3, "]": 57, "}": 1197, ">": 25137}
+CLOSING_AUTOCOMPLETE = {")": 1, "]": 2, "}": 3, ">": 4}
 
 
 def syntax_error_score(data: List[str]) -> int:
@@ -12,13 +13,44 @@ def syntax_error_score(data: List[str]) -> int:
         opened = []
 
         for char in line:
-            if char in OPENING_DELIMITERS:
-                opened.append(OPENING_DELIMITERS[char])
+            if char in OPENING:
+                opened.append(OPENING[char])
             elif opened.pop() != char:
-                result += CLOSING_DELIMITERS[char]
+                result += CLOSING_SYNTAX[char]
                 break
 
     return result
+
+
+def autocomplete_score(opened: List[str]) -> int:
+    score = 0
+
+    for delimiter in reversed(opened):
+        score *= 5
+        score += CLOSING_AUTOCOMPLETE[delimiter]
+
+    return score
+
+
+def autocomplete_score_middle(data: List[str]) -> int:
+    scores = []
+
+    for line in data:
+        opened = []
+        incomplete = True
+
+        for char in line:
+            if char in OPENING:
+                opened.append(OPENING[char])
+            elif opened.pop() != char:
+                incomplete = False
+                break
+
+        if incomplete:
+            scores.append(autocomplete_score(opened))
+
+    scores.sort()
+    return scores[len(scores) // 2]
 
 
 with open("data.txt") as f:
@@ -28,3 +60,4 @@ with open("data.txt") as f:
         data.append(line.strip())
 
     print(syntax_error_score(data))
+    print(autocomplete_score_middle(data))

263061193e033e07c438e1360be42c83e54c0c37
Šimon Brauner <simonbrauner@seznam.cz>
2022-08-09 19:01:37 +0200

    09 part 2

--- 09.py
+++ 09.py
@@ -1,4 +1,5 @@
 from typing import List
+from math import prod
 
 
 def is_lowest_point(x: int, y: int, data: List[List[int]]) -> bool:
@@ -25,6 +26,34 @@ def risk_level(data: List[List[int]]) -> int:
     return result
 
 
+def basin_size(x: int, y: int, data: List[List[int]]) -> int:
+    result = 1
+    data[y][x] = 9
+
+    if x != 0 and data[y][x - 1] != 9:
+        result += basin_size(x - 1, y, data)
+    if x != len(data[0]) - 1 and data[y][x + 1] != 9:
+        result += basin_size(x + 1, y, data)
+    if y != 0 and data[y - 1][x] != 9:
+        result += basin_size(x, y - 1, data)
+    if y != len(data) - 1 and data[y + 1][x] != 9:
+        result += basin_size(x, y + 1, data)
+
+    return result
+
+
+def basin_size_product(data: List[List[int]]) -> int:
+    sizes = []
+
+    for x in range(len(data[0])):
+        for y in range(len(data)):
+            if data[y][x] != 9:
+                sizes.append(basin_size(x, y, data))
+
+    sizes.sort()
+    return prod(sizes[-3:])
+
+
 with open("data.txt") as f:
     data = []
 
@@ -32,3 +61,4 @@ with open("data.txt") as f:
         data.append([int(x) for x in line.strip()])
 
     print(risk_level(data))
+    print(basin_size_product(data))

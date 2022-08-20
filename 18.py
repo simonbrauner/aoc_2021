09ea44940755acd149736eb0a64984dd541b4c76
Šimon Brauner <simonbrauner@seznam.cz>
2022-08-20 14:31:41 +0200

    18 classmethod

--- 18.py
+++ 18.py
@@ -7,31 +7,28 @@ class Pair:
     left: int | Pair
     right: int | Pair
 
+    @classmethod
+    def parse(cls, raw: str) -> int | Pair:
+        if raw.isnumeric():
+            return int(raw)
 
-def parse_snailfish_number(raw: str) -> int | Pair:
-    if raw.isnumeric():
-        return int(raw)
+        depth = -1
 
-    depth = -1
+        for index, char in enumerate(raw):
+            if char == "[":
+                depth += 1
+            elif char == "]":
+                depth -= 1
+            elif depth == 0 and char == ",":
+                return cls(Pair.parse(raw[1:index]), Pair.parse(raw[index + 1 : -1]))
 
-    for index, char in enumerate(raw):
-        if char == "[":
-            depth += 1
-        elif char == "]":
-            depth -= 1
-        elif depth == 0 and char == ",":
-            return Pair(
-                parse_snailfish_number(raw[1:index]),
-                parse_snailfish_number(raw[index + 1 : -1]),
-            )
-
-    assert False
+        assert False
 
 
 with open("data.txt") as f:
     numbers = []
 
     for line in f:
-        numbers.append(parse_snailfish_number(line.strip()))
+        numbers.append(Pair.parse(line.strip()))
 
     print(numbers)

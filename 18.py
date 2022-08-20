77546d4d75596427ec75b90dfb79dd3a68a67829
Šimon Brauner <simonbrauner@seznam.cz>
2022-08-20 14:56:42 +0200

    18 set parents

--- 18.py
+++ 18.py
@@ -6,6 +6,15 @@ from dataclasses import dataclass
 class Pair:
     left: int | Pair
     right: int | Pair
+    parent: None | Pair = None
+    is_left: bool = True
+
+    def become_parent(self) -> None:
+        if isinstance(self.left, Pair):
+            self.left.parent = self
+        if isinstance(self.right, Pair):
+            self.right.parent = self
+            self.right.is_left = False
 
     @classmethod
     def parse(cls, raw: str) -> int | Pair:
@@ -20,7 +29,9 @@ class Pair:
             elif char == "]":
                 depth -= 1
             elif depth == 0 and char == ",":
-                return cls(Pair.parse(raw[1:index]), Pair.parse(raw[index + 1 : -1]))
+                new = cls(Pair.parse(raw[1:index]), Pair.parse(raw[index + 1 : -1]))
+                new.become_parent()
+                return new
 
         assert False
 

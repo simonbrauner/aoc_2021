2bcbcf89056855623b7134206358e32f8622aee5
Šimon Brauner <simonbrauner@seznam.cz>
2022-08-28 09:19:10 +0200

    19 input

--- 19.py
+++ 19.py
@@ -1,5 +1,9 @@
-def all_orientations(x: int, y: int, z: int) -> set[tuple[int, int, int]]:
+Coordinates = tuple[int, int, int]
+
+
+def all_orientations(coordinates: Coordinates) -> set[Coordinates]:
     result = set()
+    x, y, z = coordinates
 
     for _ in range(3):
         result.add((x, y, z))
@@ -14,3 +18,18 @@ def all_orientations(x: int, y: int, z: int) -> set[tuple[int, int, int]]:
         x, y, z = y, z, x
 
     return result
+
+
+with open("data.txt") as f:
+    data: list[list[Coordinates]] = []
+    scanner_index = 0
+
+    while f.readline().strip() != "":
+        data.append([])
+        while (line := f.readline().strip()) != "":
+            coordinates = [int(x) for x in line.split(",")]
+            assert len(coordinates) == 3
+            data[scanner_index].append((coordinates[0], coordinates[1], coordinates[2]))
+        scanner_index += 1
+
+    print(data)
